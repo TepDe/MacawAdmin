30aee1d5489a1f3198e810f411adda091d461b40
SoftwaveDev-FreeLancer <freelancer.swdev@gmail.com>
2025-07-20 10:10:33 +0700

    s

--- src/app/primary-component/topbar/topbar.ts
+++ src/app/primary-component/topbar/topbar.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {RouterModule} from '@angular/router';
+import {MatButton} from '@angular/material/button';
+import {FormControlDirective, InputGroupComponent} from '@coreui/angular';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    RouterModule,
+    MatButton,
+    InputGroupComponent,
+    FormControlDirective,
+    NgOptimizedImage
+  ],
   standalone: true,
 
   templateUrl: './topbar.html',
